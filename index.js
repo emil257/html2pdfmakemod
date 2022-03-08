5de0ed57faa37e4ebdbccae052aa68fd3f6ec6c2
emil257 <emil.saxlund@live.se>
2022-03-08 11:40:09 +0100

    emil

--- index.js
+++ index.js
@@ -656,7 +656,7 @@ function htmlToPdfMake(htmlText, options) {
             else {
               value = _this.convertToUnit(value);
             }
-            ret.push({key:"lineHeight", value:value - 0.3});
+            ret.push({key:"lineHeight", value:value - 0.4});
             break;
           }
           case "text-align": {
