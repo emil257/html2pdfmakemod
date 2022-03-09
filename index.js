56290d6bfa8ecbee914313ece7e8cdccca3ea114
emil257 <emil.saxlund@live.se>
2022-03-09 12:06:28 +0100

    lineheightfix

--- index.js
+++ index.js
@@ -670,7 +670,7 @@ function htmlToPdfMake(htmlText, options) {
             let itemHeight = parseInt(value) * fontSize
             let spacing = itemHeight - fontSize
             if(spacing > 0) ret.push({key:"absolutePosition", value:{x:0, y: spacing/2}})
-
+            ret.push({key:"absolutePosition", value:{x:0, y: 5}})
             ret.push({key:"lineHeight", value:value});
             break;
           }
